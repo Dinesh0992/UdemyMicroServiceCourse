d7dd4ec8a0098fc515bdcd998dc9ea0b58e33d1e
Dinesh vaddn <dineshvaddn@outlook.com>
2025-07-06 09:41:06 +0530

    ADDED 29.Swagger, 30.Angular Client APP for USerSErvice  and Started  33.Creating Product Microservice

---
+++ eCommerceSolution.ProductsService/BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.ProductService.BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+        {
+            // TO DO : Register your business logic layer services here
+            return services;
+        }
+    }
+}
---
+++ eCommerceSolution.ProductsService/DataAccessLayer/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.ProductService.DataAccessLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDataAccessLayer(this IServiceCollection services)
+        {
+            // TO DO : Register your data access layer services here
+            return services;
+        }
+    }
+}
